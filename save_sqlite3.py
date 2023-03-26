7c474efa2365d87159eb314747521532445597c8
zeppfreak <zeppfreak@gmail.com>
2023-03-26 12:04:37 +0900

    fix setup.sh for mongodb setup

--- save_sqlite3.py
+++ save_sqlite3.py
@@ -1,21 +1,23 @@
 import sqlite3
 
-conn = sqlite3.connect('top_cities.db')
+conn = sqlite3.connect("top_cities.db")
 c = conn.cursor()
-c.execute('DROP TABLE IF EXISTS cities')
-c.execute("""
+c.execute("DROP TABLE IF EXISTS cities")
+c.execute(
+    """
   CREATE TABLE cities (
     rank integer,
     city text,
     population integer
   )
-""")
+"""
+)
 
-c.execute('INSERT INTO cities VALUES (?, ?, ?)', (1, '上海', 24150000))
+c.execute("INSERT INTO cities VALUES (?, ?, ?)", (1, "上海", 24150000))
 
-c.execute('SELECT * FROM cities')
+c.execute("SELECT * FROM cities")
 for row in c.fetchall():
-  print(row)
+    print(row)
 
 
 conn.close()
--- setup.sh
+++ setup.sh
@@ -1,3 +1,14 @@
-sudo apt-get install python3-venv 
-sudo apt-get install sqlite3
+# install mongodb
+## mac
+brew tap mongodb/brew
+brew install mongodb-community
+
+sudo mkdir -p /usr/local/var/mongodb
+sudo chown -R $USER /usr/local/var/mongodb
+sudo mkdir -p /usr/local/var/log/mongod
+sudo chown -R $USER /usr/local/var/log/mongodb
+vim /usr/local/etc/mongod.conf # set systemLog.destination and storage.dbPath
+
+# install python library
 pip install flake8 black isort mypy
+pip install pymongo
