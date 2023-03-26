d1832f15876efa3295e9a856abc39ecdb4440b4f
zeppfreak <aoi@instance-1.asia-northeast1-b.c.ornate-course-263801.internal>
2023-03-26 01:47:55 +0000

    add script for sqlite

---
+++ save_sqlite3.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+conn = sqlite3.connect('top_cities.db')
+c = conn.cursor()
+c.execute('DROP TABLE IF EXISTS cities')
+c.execute("""
+  CREATE TABLE cities (
+    rank integer,
+    city text,
+    population integer
+  )
+""")
+
+c.execute('INSERT INTO cities VALUES (?, ?, ?)', (1, '上海', 24150000))
+
+c.execute('SELECT * FROM cities')
+for row in c.fetchall():
+  print(row)
+
+
+conn.close()
