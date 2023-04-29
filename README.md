2c95f872436f7e4144460a10649ce9fb97ebcb40
zeppfreak <zeppfreak@gmail.com>
2023-04-29 12:48:45 +0900

    add youtube data api sample

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 ```sh
 mongod --config /usr/local/etc/mongod.conf
 ```
+
+## pip
+```sh
+pip install google-api-python-client
+```
---
+++ search_youtube_videos.py
@@ -0,0 +1,16 @@
+import os
+
+from apiclient.discovery import build
+
+YOUTUBE_API_KEY = os.environ['YOUTUBE_API_KEY']
+
+youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
+
+search_response = youtube.search().list(
+    part='snippet',
+    q='手芸',
+    type='video'
+).execute()
+
+for item in search_response['items']:
+    print(item['snippet']['title'])
